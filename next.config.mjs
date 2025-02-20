7f73d9a349e2b2c563d3f9be12391213c2324c85
abolfazlsli <140882091+abolfazlsli@users.noreply.github.com>
2025-02-20 15:08:31 +0330

    added PWA config and change port to 80

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from 'next-pwa'
 
-const nextConfig = {};
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: false,
+  skipWaiting: true,
+  // any other next-pwa options you may have
+});
+
+
+const nextConfig = withPWA({});
 
 export default nextConfig;
