4290cf172682a9e7e805f93746244a62eadea079
abolfazlsli <140882091+abolfazlsli@users.noreply.github.com>
2025-04-08 14:13:11 +0330

    added form fild system

--- src/app/dashboard/shops/(actions)/@shop/addcar/page.js
+++ src/app/dashboard/shops/(actions)/@shop/addcar/page.js
@@ -8,199 +8,190 @@ import {
     DialogDescription,
     DialogHeader,
     DialogTitle,
-  } from "@/components/ui/dialog"
-
-  import { Badge } from "@/components/ui/badge" 
-
-  import {
+} from "@/components/ui/dialog"
+import { Badge } from "@/components/ui/badge"
+import {
+    Form,
+    FormItem,
+    FormLabel,
+    FormControl,
+    FormDescription,
+    FormMessage,
+    FormField,
+} from "@/components/ui/form"
+import {
     Select,
     SelectContent,
     SelectItem,
     SelectTrigger,
     SelectValue,
-  } from "@/components/ui/select" 
-
-
+} from "@/components/ui/select"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+import { Input } from "@/components/ui/input"
 
-
-const AddCarPage  = () => {
+const AddCarPage = () => {
+  const form = useForm()
   const router = useRouter()
-  const fildsdata = useState()
-  const [pagedata , setPageData] = useState(
-    {
-      brands : [] ,
-      filds : null ,
-      senders : null ,
-      setep : 1
-    }
-  )
-  useEffect(
-    () => {
-      GetCarsBrands().then(
-        res => {
-          
-          setPageData(
-            {
-              ...pagedata , brands : res.data.apidata
-            }
-          )
-        }
-      )
-    } , []
-  )
+  const [pagedata, setPageData] = useState({
+    brands: [],
+    filds: null,
+    setep: 1 , 
+    senders : null
+  })
+
+  useEffect(() => {
+    GetCarsBrands().then(res => {
+      setPageData({
+        ...pagedata,
+        brands: res.data.apidata
+      })
+    })
+  }, [])
+
+  const sendHandler = (d) => {
+    const data = d
+    console.log(data)
+  }
   const handelClose = () => {
     router.back()
   }
+
   const changeBrandHandler = (e) => {
-    getBrandFilds({"brandid" : e}).then(
-      res => {
-        let tempdata = []
-          res.data.data.map(
-            d => {
-              console.log("d : " ,d)
-              tempdata.push(
-                {
-                  name : d.fieldname ,
-                  value : "" ,
-                  type : d.fieldtype , 
-                  label : d.fieldlabel
-                }
-              )
-            }
-          )
-         setPageData(
-        {
-          ...pagedata , filds : res.data.data , senders : tempdata
-        }
-      )
-      }
-    )
-     
+    getBrandFilds({ "brandid": e }).then(res => {
+      setPageData({
+        ...pagedata,
+        filds: res.data.data,
+
+      })
+    })
   }
-    return (
-        <div>
-          {pagedata.setep === 1 ? <>
-            <Dialog open={true} onOpenChange={handelClose}>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>
-                      افزودن خودرو
-                  </DialogTitle>
-                  <DialogDescription>
-                      <Select onValueChange={(e) => changeBrandHandler(e)}>
+
+  return (
+    <div>
+      {pagedata.setep === 1 ? (
+        <Dialog open={true} onOpenChange={handelClose}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>افزودن خودرو</DialogTitle>
+              <DialogDescription>
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(sendHandler)}>
+                    <Select onValueChange={(e) => changeBrandHandler(e)}>
+                      <div className="pt-[10px] pb-[10px]">
+                        <SelectTrigger className="rounded-full text-right">
+                          <SelectValue placeholder="ماشینت چیه ؟" />
+                        </SelectTrigger>
+                      </div>
+                      <SelectContent>
                         <div className="pt-[10px] pb-[10px]">
-                          <SelectTrigger className="rounded-full text-right">
-                            <SelectValue placeholder="ماشینت چیه ؟" />
-                          </SelectTrigger>
+                          {pagedata.brands.map((e, index) => (
+                            <SelectItem value={e.value} key={index}>
+                              {e.label}
+                            </SelectItem>
+                          ))}
                         </div>
-                        <SelectContent>
-                          <div className="pt-[10px] pb-[10px]">
-                            {
-                              pagedata.brands.map(
-                                (e , index) => (
-                                  <SelectItem value={e.value} key={index}>
-                                    {e.label}
-                                  </SelectItem>
-                                )
-                              )
-                            }
-                          </div>
-                        </SelectContent>
-                      </Select>
-                      {
-                        pagedata.filds ? 
-                        pagedata.filds.map(
-                          (e , index) => (
-                              e.fieldtype === "select" ?
-                              <div key={index}>
-                              <h1 className="text-right w-[100%] pr-[10px]">
-                                {e.fieldlabel}
-                              </h1>
-                               <Select>
-                                <div className="pt-[10px] pb-[10px]">
-                                  <SelectTrigger className="rounded-full text-right">
-                                    <SelectValue placeholder={e.fieldlabel} />
+                      </SelectContent>
+                    </Select>
+
+                    {pagedata.filds &&
+                      pagedata.filds.map((f, index) =>
+                        f.fieldtype === "select" ? (
+                          <div key={index} className="mt-[10px] mb-[10px]">
+                            <h1 className="text-right mt-[5px] mb-[5px]">
+                              {f.fieldlabel}
+                            </h1>
+                            <FormField
+                              control={form.control}
+                              name={f.fieldname}
+                              render={({ field }) => (
+                                <Select {...field} onValueChange={field.onChange}>
+                                  <SelectTrigger className="rounded-3xl">
+                                    <SelectValue placeholder={f.fieldlabel} />
                                   </SelectTrigger>
-                                </div>
-                                <SelectContent>
-                                {console.log(e.options)}
-                                  <div className="pt-[10px] pb-[10px]">
-                                    {
-                                      e.options.map(
-                                        event => (
-                                          <SelectItem value={event.value}>
-                                            
-                                            {event.text}
-                                          </SelectItem>
-                                        )
-                                      )
-                                    }
-                                  </div>
-                                </SelectContent>
-                              </Select>
-                              </div> : e.fieldtype === "color" ? <>
-                                <h1 className="text-right w-[100%] pr-[10px]">
-                                  رنگ
+                                  <SelectContent>
+                                    {(f.options || []).map((option, i) => (
+                                      <SelectItem value={option.value} key={i}>
+                                        {option.text}
+                                      </SelectItem>
+                                    ))}
+                                  </SelectContent>
+                                </Select>
+                              )}
+                            />
+                          </div>
+                        ) : f.fieldtype === "color" ? 
+                        <div key={index}>
+                        <FormField 
+                          control = {form.control} 
+                          name={f.fieldname}
+                          render = {({ field }) => (
+                            <>
+                              <h1 className="text-right">
+                                  {
+                                    f.fieldlabel
+                                  }
                                 </h1>
-                                <div className="flex w-[100%] justify-center items-center">
-                                      {
-                                        e.options.map(
-                                          (color , index) => (
-                                            <>
-                                            <div className={`w-[50px] h-[50px] rounded-full m-[10px] shadow-md ring-1 ring-gray-200 cursor-pointer ${pagedata.senders.filter(
-                                              item => item.name === "color" 
-                                            ).value ===color.value ? "ring-4" : "" }` } style={{backgroundColor:color.value}} onClick={e => setPageData({...pagedata , senders : [...pagedata.senders , {name : "color" , value : color.value , type : "color" , label : "رنگ" }]})}></div>
-                                              {/* <Badge className={`bg-${color.value}-400`} key={index}>
-                                                {
-                                                  color.text
-                                                }
-                                              </Badge> */}
-                                            </>
-                                          )
-                                        )
-                                      }
-                                  </div>
-                              </> : <></>
-                          )
-                        )
-                        : <></>
-                      }
-                      <div className="flex justify-center items-center">
-                        <Button className="bg-amber-500 w-[100%] rounded-full mt-[20px]" onClick = {e => {console.log(pagedata)}}>
-                          گام بعد
-                        </Button>
-                      </div>
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
-          </> : pagedata.setep === 2 ? <>
-          <Dialog open={true} onOpenChange={handelClose}>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>
-                      افزودن خودرو
-                  </DialogTitle>
-                  <DialogDescription>
-                      <Select onValueChange={(e) => changeBrandHandler(e)}>
-                      </Select>
-                      <div className="flex justify-center items-center">
-                        <Button className="bg-amber-500 w-[100%] rounded-full mt-[20px]" onClick = {e => {console.log(pagedata)}}>
-                          گام بعد
-                        </Button>
-                      </div>
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
-          </> : <></>}
-           
+                              <div className="w-[100%] flex justify-center items-center flex-wrap">
+                              <Select onValueChange={field.onChange}>
+                                  <SelectTrigger className="items-center rounded-2xl">
+                                    <SelectValue placeholder="رنگ" />
+                                  </SelectTrigger>
+                                  <SelectContent >
+                                {f.options.map(
+                                  (color , index) =>
+                                  (
+                                    <SelectItem value={color.value}>
+                                      <div className="flex justify-center items-center cursor-pointer w-[100%]">
+                                        <div className={`w-[15px] h-[15px] rounded-full m-[10px] ring-2 ring-gray-300 shadow-sm cursor-pointer`} style={{backgroundColor : color.value }} key={index}></div>
+                                        <h1 className="m-0 p-0">
+                                          {color.text}
+                                        </h1>
+                                      </div>
+                                    </SelectItem>
+                                  )
+                                )}
+                                  </SelectContent>
+                                  </Select>
+                                </div>
+                            </>
+                          )}
+                        />
+                        </div>
+                         : null
+                      )}
 
-        </div>
-    )
+                    <div className="flex justify-center items-center">
+                      <Button
+                        className="bg-amber-500 w-[100%] rounded-full mt-[20px]"
+                        onClick={(e) => {
+                          console.log(pagedata)
+                        }}
+                      >
+                        گام بعد
+                      </Button>
+                    </div>
+                  </form>
+                </Form>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      ) : pagedata.setep === 2 ? (
+        <Dialog open={true} onOpenChange={handelClose}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>افزودن خودرو</DialogTitle>
+              <DialogDescription>
+                <Select onValueChange={(e) => changeBrandHandler(e)}></Select>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      ) : null}
+    </div>
+  )
 }
-
 
 export default AddCarPage
